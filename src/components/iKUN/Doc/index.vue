cefa6df06fda78809060e92392a9e6ca3a99628b
lizhuo <2541778295@qq.com>
2023-02-14 20:19:57 +0800

    2.14

---
+++ src/components/iKUN/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iKUNDoc',
+  data() {
+    return {
+      url: 'https://yhx1001.github.io/iKUN_docsify/#/'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/iKUN/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iKUNGit',
+  data() {
+    return {
+      url: 'https://github.com/yhx1001/iKUN_Graduation_Project'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
